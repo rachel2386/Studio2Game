80b6bad1c3b39d783d9e7ee89cd2ae072fc6bfac
Rachel Wang <rhw280@nyu.edu>
2019-02-14 16:32:45 -0500

    added a fleeing NPC

---
+++ Assets/Scripts/NPCTest/DetectNPC.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DetectNPC : MonoBehaviour
+{
+    private NavMeshAgent _agent;
+    public GameObject Player;
+    public float EnemyDistanceRun = 4.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position, Player.transform.position);
+        Debug.Log("Distance: " + distance);
+        //run away from player
+        if (distance < EnemyDistanceRun)
+        {
+            //vector player to me
+            Vector3 dirtoPlayer = transform.position - Player.transform.position;
+            Vector3 newPos = transform.position + dirtoPlayer;
+            _agent.SetDestination(newPos);
+        }
+    }
+}
