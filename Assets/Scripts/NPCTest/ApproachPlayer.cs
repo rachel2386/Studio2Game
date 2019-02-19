ee5bfae6d85f2773b1b79b5adf89200dfe9a5976
Rachel Wang <rhw280@nyu.edu>
2019-02-19 16:12:34 -0500

    added approaching NPC

---
+++ Assets/Scripts/NPCTest/ApproachPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApproachPlayer : MonoBehaviour
+{
+    public GameObject ThePlayer;
+    public float FollowSpeed;
+
+    void LateUpdate()
+    {
+        var target = Camera.main.transform.position;
+        if (Vector3.Distance(transform.position, target) < 5)
+        {
+            FollowSpeed = 0.1f;
+            transform.position = Vector3.MoveTowards(transform.position, ThePlayer.transform.position, FollowSpeed);
+            // ThePlayer.velocity = Vector3.zero
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
