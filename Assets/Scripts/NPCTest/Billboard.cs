9cec83161516df7c571d73a42eb7e17854845563
Rachel Wang <rhw280@nyu.edu>
2019-02-19 15:37:02 -0500

    added NPCs in corridor added SFX

---
+++ Assets/Scripts/NPCTest/Billboard.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    void LateUpdate()
+         {
+             var target = Camera.main.transform.position;
+             if (Vector3.Distance(transform.position, target) < 3){
+                 target.y = transform.position.y;
+                 transform.LookAt(target);
+             }
+            
+         }
+}
