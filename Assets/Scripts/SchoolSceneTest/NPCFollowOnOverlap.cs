935bcd0c8c6503f40d5aa6b2cf437ff851b04bb2
rachelqxl <rl2563@nyu.edu>
2019-02-21 18:24:47 -0500

    NPCfollow script with sphereOverlap so that npc starts to follow player when detected in a certain radius. add playerBase to FPScontroller prefab as target for NPC follow move Ethan position in scene

---
+++ Assets/Scripts/SchoolSceneTest/NPCFollowOnOverlap.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCFollowOnOverlap : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Transform playerTransform;
+    private Transform NPCTransform;
+    bool seePlayer = false;
+   
+    void Start()
+    {
+        playerTransform = GameObject.Find("PlayerBase").transform;
+        NPCTransform = transform;
+    }
+
+    // Update is called once per frame
+    float NPCx = 0f;
+    float NPCz = 0f;
+    Vector3 newNPCPos = new Vector3(0, 0, 0);
+    void Update()
+    {
+        
+        if (DetectPlayer())
+        {
+           
+            if (Vector3.Distance(NPCTransform.position, playerTransform.position) >= 0.2f)
+            {
+                 NPCx= Mathf.MoveTowards(NPCTransform.position.x, playerTransform.position.x, 1f * Time.deltaTime);  
+                 NPCz= Mathf.MoveTowards(NPCTransform.position.z, playerTransform.position.z -playerTransform.localScale.z,1f * Time.deltaTime);  
+            }
+            else
+            {
+                
+                NPCx = NPCTransform.position.x;
+                NPCz = NPCTransform.position.z;
+
+            }
+
+            newNPCPos.x = NPCx;
+            newNPCPos.z = NPCz;
+            newNPCPos.y = NPCTransform.position.y;
+            NPCTransform.position = newNPCPos;
+
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (NPCTransform != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(NPCTransform.position,1);
+        }
+    }
+
+    bool DetectPlayer()
+    {
+       
+        Collider[] stuffInSphere = Physics.OverlapSphere(NPCTransform.position, 1.5f);
+
+        for (int i = 0; i < stuffInSphere.Length; i++)
+        {
+            if (stuffInSphere[i].CompareTag("Player"))
+            {
+                seePlayer = true;
+            }
+        }
+
+        return seePlayer;
+    }
+}
