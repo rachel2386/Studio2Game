40851a408a592284427d409c3cf5c07a7828a986
thankcreate <thankcreate@gmail.com>
2019-03-01 00:50:02 -0500

    1. Door scene added a pseudo game object solution to simulate the door without changing the model prefab structure 2. Added a tool to automatically attach colliders to mesh recursively. 3. Added a gitignore to ignore the playmaker setting.

---
+++ Assets/Tools/AddCollider.cs
@@ -0,0 +1,65 @@
+﻿using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddCollider : MonoBehaviour
+{
+    public GameObject attachRoot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    [Button]
+    public void Attach()
+    {
+        RecursiveAttach(attachRoot.transform);
+    }
+
+    void RecursiveAttach(Transform parent)
+    {
+        AttachCollider(parent);
+        for (int i = 0; i < parent.childCount; i++)
+        {
+            var child = parent.GetChild(i);
+            RecursiveAttach(child);
+        }
+    }
+
+    void AttachCollider(Transform trans)
+    {
+        if (!trans)
+            return;
+
+        var meshFilter = trans.GetComponent<MeshFilter>();
+        if (!meshFilter || !trans.GetComponent<MeshRenderer>())
+            return;
+
+        var colls = trans.GetComponents<Collider>();
+
+        if (colls.Length != 0)
+            return;
+
+        if (meshFilter.name == "Cube")
+        {
+            trans.gameObject.AddComponent<BoxCollider>();
+        }
+        else
+        {
+            trans.gameObject.AddComponent<MeshCollider>();
+
+        }
+        
+    }
+
+    
+}
---
+++ Assets/Tools/PseudoGameObject.cs
@@ -0,0 +1,54 @@
+﻿using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PseudoGameObject : MonoBehaviour
+{
+    public GameObject[] children;
+
+    private void Awake()
+    {
+        foreach(var go in children)
+        {
+            go.transform.SetParent(this.transform);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    [Button]
+    public void TestMode()
+    {
+        foreach (var go in children)
+        {
+            var testGo = Instantiate(go, go.transform.position, go.transform.rotation, this.transform);
+            testGo.transform.localScale = go.transform.lossyScale;
+            go.SetActive(false);         
+        }
+    }
+
+    [Button]
+    public void RestoreTest()
+    {
+        foreach (var go in children)
+        {            
+            go.SetActive(true);
+        }
+
+        for(int i = transform.childCount; i > 0; i--)
+        {
+            DestroyImmediate(transform.GetChild(0).gameObject);
+        }
+    }
+}
