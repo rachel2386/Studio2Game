c5ec4622021d6dfe16d372aa5f53ff8152861028
rachelqxl <rl2563@nyu.edu>
2019-04-01 02:10:26 -0400

    added navmesh in school scene. 3 types of npcs that moves on trigger.

---
+++ Assets/1-DoubleRachel/School/Scripts/NPCWalkOnTrigger.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using HutongGames.PlayMaker.Actions;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+
+public class NPCWalkOnTrigger : MonoBehaviour
+{
+    private NavMeshAgent myAgent;
+    private Animator myAnim;
+    private Vector3 steeringTargetLastFrame;
+
+    private bool goingForward = true;
+    
+    private bool playerTriggered = false;
+
+    public bool PlayerTriggered
+    {
+        get => playerTriggered;
+        set => playerTriggered = value;
+    }
+
+    private Vector3 AgentOffset;
+    private Vector3 DestinationOffset;
+    private float turnSpeed = 2f;
+    public Transform myDestination;
+
+    [Header(" NPC Type:")]
+    public bool Trigger;
+    public bool Automatic;
+    public bool Stationary;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAgent = GetComponent<NavMeshAgent>();
+        myAnim = GetComponent<Animator>();
+        AgentOffset = transform.position;
+        DestinationOffset = myDestination.position;
+        
+        if (Stationary)
+        {
+            randomRot = transform.eulerAngles.y;
+            InvokeRepeating("generateRandomNum", 6,7);
+        }
+    }
+
+    // Update is called once per frame
+    
+    void Update()
+    {
+     
+        if (Trigger)
+        {
+           if(playerTriggered)
+            myAgent.SetDestination(myDestination.position);
+        }
+        else if (Automatic)
+        {
+             
+            myAgent.SetDestination( goingForward ? DestinationOffset : AgentOffset);
+            //walking back and forth two points
+            if ( !myAgent.pathPending && myAgent.remainingDistance <= 0.5f)
+            {
+                  goingForward = !goingForward;
+            }
+     
+        }
+        else if (Stationary)
+        {
+         AutoRotate();
+        }
+        
+        myAgent.updatePosition = false;
+        myAgent.updateRotation = true;
+        float  speed= myAgent.desiredVelocity.magnitude;
+        //update animator 
+        myAnim.SetFloat("Forward",speed);
+        // look at walking dir    
+            //transform.LookAt(myAgent.steeringTarget + transform.forward);
+    }
+
+    void AutoRotate()
+    {
+      // if(Rotating)
+     
+      transform.eulerAngles = new Vector3(transform.eulerAngles.x,
+                                            Mathf.MoveTowardsAngle(transform.eulerAngles.y, randomRot, turnSpeed), 
+                                            transform.eulerAngles.x);
+
+      float angleToTurn = randomRot - transform.eulerAngles.y;
+      if (angleToTurn >= 5)
+      {
+          myAnim.SetFloat("Turn", 0.8f);
+          myAnim.PlayInFixedTime("Grounded", 0,  degreesTurned/turnSpeed);
+      }
+      else
+         myAnim.SetFloat("Turn", 0);
+
+
+     
+    }
+
+    private void OnAnimatorMove()
+    {
+        transform.position = myAgent.nextPosition;
+       
+    }
+
+    private float randomRot;
+    private float degreesTurned;
+    
+    void generateRandomNum()
+    {
+       
+        degreesTurned =  Random.Range(30, 89);
+       randomRot = transform.eulerAngles.y + degreesTurned;
+        print("generated");
+       
+    }
+
+    
+}
---
+++ Assets/timeCountdown.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timeCountdown : MonoBehaviour
+{
+    public float time = 180;
+
+    private Text myText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myText.text = "Time Left:" + Mathf.FloorToInt(time/60f) + "m" + Mathf.RoundToInt(time%60) + "s";
+        if (time >= 0)
+        {
+            time -= Time.deltaTime;
+        }
+        else
+        {
+            time = 0;
+        }
+    }
+}
