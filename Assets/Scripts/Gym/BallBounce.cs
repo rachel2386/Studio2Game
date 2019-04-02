0163cb364b42fac9576cb68afcc07b0eac2d1299
Rachel h Wang <rhw280@nyu.edu>
2019-04-02 16:58:50 -0400

    added Gym scene & Dark Gym scene

---
+++ Assets/Scripts/Gym/BallBounce.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBounce : MonoBehaviour
+{
+
+    public AudioClip[] tones;
+	
+    public float maxSpeed = 1.0f;
+
+    private AudioSource _audioSource;
+
+    void Start() {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        int noteIndex = GetCollisionStrength(collision);
+		
+        _audioSource.PlayOneShot(tones[noteIndex]);
+    }
+    
+    int GetCollisionStrength(Collision collision)
+    {
+        //project the velocity onto the normal of the platform
+        Vector3 normal = collision.contacts[0].normal;
+        Vector3 bounceAmount = Vector3.Project(collision.relativeVelocity, normal);
+        float speed = bounceAmount.magnitude;
+		
+        //we want to map the ball's speed to a 0-1 range
+        float clampedSpeed = Mathf.Clamp(speed / maxSpeed, 0.0f, 0.99f);
+
+        //we want to round down in order to get the correct index of our tone array
+        return Mathf.FloorToInt(clampedSpeed * tones.Length);
+    }
+//    public AudioClip bounce;
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        GetComponent<AudioSource>().playOnAwake = false;
+//        GetComponent<AudioSource>().clip = bounce;
+//    }
+//
+//    private void OnCollisionEnter(Collision other)
+//    {
+//        GetComponent<AudioSource>().Play();
+//    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Gym/BallSpawner.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BallSpawner : MonoBehaviour
+{
+
+	public Transform model;
+	public float spawnRate = 1.0f;
+	public AudioClip bell;
+	float progress = 0.0f;
+	private int count;
+
+	void Start()
+	{
+		count = 0;
+	}
+
+	void Spawn()
+	{
+		Transform next = Instantiate(model, transform.position, transform.rotation) as Transform;
+		next.parent = transform;
+		next.localPosition = Vector3.zero;
+		count += 1;
+	}
+
+	void Update()
+	{
+		progress += spawnRate * Time.deltaTime;
+		if (progress >= 1.0f)
+		{
+			Spawn();
+			progress -= 1.0f;
+		}
+
+		if (count == 10)
+		{
+			AudioSource audio = GetComponent<AudioSource>();
+			audio.clip = bell;
+			audio.Play();
+		}
+
+		IEnumerator playSoundThenLoad()
+		{
+			yield return new WaitForSeconds(26);
+			SceneManager.LoadScene("DarkGym");
+			Debug.Log("print");
+		}
+	}
+}
