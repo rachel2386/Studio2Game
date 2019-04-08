7319fb6532b1369cd230832a6a5c4467447c0131
Rachel h Wang <rhw280@nyu.edu>
2019-04-08 19:15:30 -0400

    added fade to black for gym scene

---
+++ Assets/Scripts/Gym/FadeToBlack.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class FadeToBlack : MonoBehaviour {
+
+	private float alpha;
+	
+	// Use this for initialization
+	void Start () {
+		alpha = 0.0f;		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		GetComponent<Image>().color = new Color(0,0,0,alpha);
+		if (alpha < 1.5)
+		{
+			alpha += 0.01f;
+		}	
+		
+		if (alpha >= 1.5)
+		{
+			SceneManager.LoadScene (0);
+			Debug.Log("Loaded Scene 0");
+
+		}	
+
+		
+	}
+}
