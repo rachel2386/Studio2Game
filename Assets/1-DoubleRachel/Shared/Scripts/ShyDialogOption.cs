d644f75a775602fe20c72bbe7c60ebfa29c27510
TronTron <thankcreate@gmail.com>
2019-04-23 12:41:48 -0400

    all set

---
+++ Assets/1-DoubleRachel/Shared/Scripts/ShyDialogOption.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShyDialogOption : MonoBehaviour
+{
+    Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponentInChildren<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public string enterChangeText = "";
+    public void PointerEnter()
+    {
+        if(enterChangeText != "")
+            text.text = enterChangeText;
+
+        enterChangeText = "";
+    }
+}
