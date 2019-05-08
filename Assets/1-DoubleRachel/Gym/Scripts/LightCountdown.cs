99d6673cacf55413d573c96e2d4b322e486cb1dd
Rachel h Wang <rhw280@nyu.edu>
2019-05-08 17:30:40 -0400

    new dark gym

---
+++ Assets/1-DoubleRachel/Gym/Scripts/LightCountdown.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightCountdown : MonoBehaviour
+{
+    public AudioSource panic;
+    public AudioSource lightswitch;
+    public Light door;
+    // Start is called before the first frame update
+    void Start()
+    {
+        door.enabled = false;
+        panic.Play();
+        StartCoroutine(LightWait());
+    }
+
+    IEnumerator LightWait()
+    {
+        yield return new WaitUntil(() => panic.isPlaying == false);
+        yield return new WaitForSeconds(3);
+        lightswitch.Play();
+        door.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
