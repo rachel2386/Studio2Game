5b9ea73e08fb203d7bb01f745663666e4244dea6
Rachel h Wang <rhw280@nyu.edu>
2019-05-08 14:46:27 -0400

    123

---
+++ Assets/1-DoubleRachel/Gym/Scripts/AudioArray.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioArray : MonoBehaviour
+{
+    public AudioClip[] sounds;
+
+    private AudioSource _audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    public void PickUpSound()
+    {
+        int noteIndex = Random.Range(0, 3);
+        _audioSource.PlayOneShot(sounds[noteIndex]);
+    }
+}
