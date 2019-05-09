4ae197e6c9cbbf9665f0b866fd00c0eb53c2e936
Rachel h Wang <rhw280@nyu.edu>
2019-05-09 01:26:00 -0400

    Dark Gym maybe completed

---
+++ Assets/1-DoubleRachel/Gym/Scripts/PlayerReposition.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerReposition : MonoBehaviour
+{
+    public Transform Position;
+    public GameObject Player;
+    public AudioSource StaticSound;
+//    public ShyCamera Camera;
+//    public float Timer = 1f;
+//    private bool Countdown;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+//            Countdown = true;
+            StaticSound.Play();
+            Player.transform.position = Position.position;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+//        Camera.runtimeProfile.grain.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+//        if (Countdown)
+//        {
+//            if (Timer > 0)
+//            {
+//                Timer-=Time.deltaTime;
+//                Camera.runtimeProfile.bloom.enabled = true;
+//            }
+//            else
+//            {
+//                Countdown = false;
+//            }
+//        }
+//        else
+//        {
+//            Camera.runtimeProfile.grain.enabled = false;
+//            Timer = 1;
+//        }
+    }
+}
